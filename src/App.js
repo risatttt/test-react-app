852e7a852caca779b24ef758d0e50ec0d558b50b
里咲 <risa@MacBook-Air.local>
2024-03-17 17:19:38 +0900

    環境変数設定

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 
+const value = process.env.TEST_SECRET ?? "no TEST_SECRET";
+const text = process.env.TEXT ?? "no TEXT";
+
 function App() {
   return (
     <div className="App">
@@ -9,14 +12,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          Value : {value}<br/>
+          Text : {text}
       </header>
     </div>
   );
